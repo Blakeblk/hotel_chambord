f413f69d2db67c9184bf2aa5455ff500a576249c
AMDIOP <alexpro.diop@gmail.com>
2023-02-01 13:59:46 +0100

    projet presque fini manque juste quelques finitions

--- js/index.js
+++
@@ -1,6 +0,0 @@
-/*==================== MENU SHOW Y HIDDEN ====================*/
-const navMenu = document.getElementById('nav-menu'),
-navToggle = document.getElementById('nav-toggle'),
-navClose = document.getElementById('nav-close');
-
-
